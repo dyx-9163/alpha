ddaa33c25b4c01e2e42768a69d44172369fec80b
dyx <853207849@qq.com>
2026-07-27 09:10:59 +0800

    feat: expose runtime diagnostic export api

--- backend/internal/httpapi/aifar_runtime_controller.go
+++ backend/internal/httpapi/aifar_runtime_controller.go
@@ -2,16 +2,38 @@ package httpapi
 
 import (
 	"aifar-deployment/backend/internal/rbac"
+	"aifar-deployment/backend/internal/store"
+	"aifar-deployment/backend/internal/worker"
 
 	"github.com/go-chi/chi/v5"
 )
 
 type aifarRuntimeController struct {
 	*API
+	startExistingTask       func(store.Task, string, worker.Job) (store.Task, error)
+	storeDiagnosticTaskPlan func(string, string, []simpleTaskStep) error
 }
 
 func newAIFARRuntimeController(api *API) *aifarRuntimeController {
 	return &aifarRuntimeController{API: api}
+}
+
+func (a *aifarRuntimeController) startExistingWithLanguage(task store.Task, lang string, job worker.Job) (store.Task, error) {
+	if a.startExistingTask != nil {
+		return a.startExistingTask(task, lang, job)
+	}
+	return a.tasks.StartExistingWithLanguage(task, lang, job)
+}
+
+func (a *aifarRuntimeController) storeDiagnosticPlanOrDelete(taskID, target string, steps []simpleTaskStep) error {
+	if a.storeDiagnosticTaskPlan != nil {
+		if err := a.storeDiagnosticTaskPlan(taskID, target, steps); err != nil {
+			_ = a.store.DeleteTask(taskID)
+			return err
+		}
+		return nil
+	}
+	return a.storeTaskPlanOrDelete(taskID, simpleTaskPlan(target, steps))
 }
 
 func (a *aifarRuntimeController) mount(r chi.Router) {
@@ -23,6 +45,11 @@ func (a *aifarRuntimeController) mount(r chi.Router) {
 	r.Post("/containers/aifar/runtime/restart-all", a.requirePermission(rbac.AppsManage, a.restartAll))
 	r.Post("/containers/aifar/runtime/cleanup-stale", a.requirePermission(rbac.AppsManage, a.cleanupStale))
 	r.Post("/containers/aifar/runtime/uninstall-agent", a.requirePermission(rbac.AppsManage, a.uninstallAgent))
+	r.Post("/containers/aifar/runtime/diagnostics/estimate", a.requirePermission(rbac.AppsManage, a.estimateDiagnostics))
+	r.Post("/containers/aifar/runtime/diagnostics/exports", a.requirePermission(rbac.AppsManage, a.createDiagnosticExport))
+	r.Get("/containers/aifar/runtime/diagnostics/exports", a.requirePermission(rbac.AppsManage, a.listDiagnosticExports))
+	r.Get("/containers/aifar/runtime/diagnostics/exports/{id}/download", a.requirePermission(rbac.AppsManage, a.downloadDiagnosticExport))
+	r.Delete("/containers/aifar/runtime/diagnostics/exports/{id}", a.requirePermission(rbac.AppsManage, a.deleteDiagnosticExport))
 	r.Post("/containers/aifar/services/install", a.requirePermission(rbac.AppsManage, a.installServices))
 	r.Post("/containers/aifar/services/{service}/scale-out", a.requirePermission(rbac.AppsManage, a.scaleOut))
 	r.Post("/containers/aifar/services/{service}/scale-in", a.requirePermission(rbac.AppsManage, a.scaleIn))
